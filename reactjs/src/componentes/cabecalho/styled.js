200d9de42df5ebfb7d7e13be55a829e5bbed374e
Gabriel <bielmelodossantos8@gmail.com>
2021-09-25 12:37:48 -0300

    teste

--- reactjs/src/componentes/cabecalho/styled.js
+++ reactjs/src/componentes/cabecalho/styled.js
@@ -91,12 +91,14 @@ const Container = styled.div`
         flex-direction: column;
         background-color: #000;
         margin-top: 30px;
+        justify-content: space-between;
 
         .logo { display: none; }
         .parte2 { display: none; }
         .logo-celular { display: flex; flex-direction: row; justify-content: space-between; }
         .barra-pesq { margin-left: 75px; margin-top: 8px;}
-        .parte-celular { display: flex; flex-direction: row; text-align: center; margin-top: 30px; }
+        .princ { width: 10px; }
+        .parte-celular { display: flex; flex-direction: row; text-align: center; margin-top: 30px; justify-content: space-between; }
         .usuario { display: none; }
         .troco { font-size: 0.8em; font-weight: bold;width: 100px; margin: 0px 60px;}
         .sair { font-size: 0.8em; margin: 0px; width: 60px; }
