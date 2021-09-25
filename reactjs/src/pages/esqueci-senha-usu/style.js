99604f185ab3df406e4203ded204ecb78afe39d1
Gabriel <bielmelodossantos8@gmail.com>
2021-09-25 15:49:08 -0300

    ok

--- reactjs/src/pages/esqueci-senha-usu/style.js
+++ reactjs/src/pages/esqueci-senha-usu/style.js
@@ -12,7 +12,7 @@ const Container = styled.div `
         display: flex;
         flex-direction: column;   
         align-items: center;
-        height: 100%;
+        height: 100vh;
         padding: 3.5em 0em;
     }
 `;
