413e862b717077564698afae1079344a45b8df19
Gabriel <bielmelodossantos8@gmail.com>
2021-09-30 09:44:44 -0300

    sdfsdfgsdfgsghsdghsghsfghdfgjefgh

--- reactjs/src/pages/tela inicial/index.js
+++ reactjs/src/pages/tela inicial/index.js
@@ -15,7 +15,7 @@ export default function TelaInicial(){
         <Container>
             <Cabecalho/>
             <div className="conteiner-ti">
-            <img src={capaMaior} width="1300em" height="600em"/>
+            <img src={capaMaior} width="1300em" height="600em" alt="" />
             <div className="titulo-ti">Acompanhe tudo sobre filmes</div>
             <div className="subTitulo-ti">A rede social para amantes de cinema</div>
             <div className="conteudo-box-ti">
