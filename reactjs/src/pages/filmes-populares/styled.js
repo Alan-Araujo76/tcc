3bf920515c78bacf0f8abd9a3136710205b42bcc
Gabriel <bielmelodossantos8@gmail.com>
2021-09-30 11:22:16 -0300

    ok

--- reactjs/src/pages/filmes-populares/styled.js
+++ reactjs/src/pages/filmes-populares/styled.js
@@ -48,6 +48,7 @@ const Container = styled.div`
 
         .titulo {
             margin: 70px 0px 50px 0px;
+            font-size: 150%;
         }
     }
 `;
@@ -62,12 +63,11 @@ const Bloco1 = styled.div`
 
     .nome {
         font-size: 17px;
-        width: 100px;
-        text-align: center;
+        width: 115px;
         margin-top: 8px;
         line-height: 22px;
         font-weight: 500;
-        margin-left: 10px;
+        margin-bottom: 20px;
     }
 
     img {
@@ -75,10 +75,11 @@ const Bloco1 = styled.div`
         height: 170px;
     }
 
-
     @media (max-width: 800px) {
+        margin: 0% 10%;
         .filme-d { display: none; }
     }
+    
     
 `;
 
@@ -87,16 +88,20 @@ const Bloco2 = styled.div`
     flex-direction: row;
     margin: 60px 140px 00px 140px;
     justify-content: space-between;
+
+    .filme { 
+        width: 115px;
+        text-align: center;
+    }
     
 
     .nome {
-        margin-left: 10px;
         font-size: 17px;
-        width: 100px;
-        text-align: center;
+        width: 115px;
         margin-top: 8px;
         line-height: 22px;
         font-weight: 500;
+        margin-bottom: 25%;
     }
 
     img {
@@ -105,6 +110,7 @@ const Bloco2 = styled.div`
     }
 
     @media (max-width: 800px) {
+        margin: 0% 10%;
         .filme-d { display: none; }
     }
     
